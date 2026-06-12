c90c97e23b2522b0e3e4979ef01c270333166bad
Yuxin Zhang <zhang@aviselabs.de>
2026-06-12 15:51:43 +0200

    finished collectResult and calculate etc

--- src/GameStatistics.cpp
+++ src/GameStatistics.cpp
@@ -19,19 +19,27 @@ GameStatistics::GameStatistics() {
 }
 
 void GameStatistics::setTrackingEnabled(bool enabled) {
-  throw std::logic_error("GameStatistics::setTrackingEnabled is not implemented yet.");
+  m_trackingEnabled = enabled;
+  // throw std::logic_error("GameStatistics::setTrackingEnabled is not implemented yet.");
 }
 
 bool GameStatistics::isTrackingEnabled() const {
-  throw std::logic_error("GameStatistics::isTrackingEnabled is not implemented yet.");
+  return m_trackingEnabled;
+  // throw std::logic_error("GameStatistics::isTrackingEnabled is not implemented yet.");
 }
 
 void GameStatistics::onGameFinished(int attempts, bool won, GameFactory::GameMode mode) {
-  throw std::logic_error("GameStatistics::onGameFinished is not implemented yet.");
+  if (m_trackingEnabled) {
+        collectResult(attempts, won, mode);
+  }
+  // throw std::logic_error("GameStatistics::onGameFinished is not implemented yet.");
+
 }
 
 void GameStatistics::collectResult(int attempts, bool won, GameFactory::GameMode mode) {
-  throw std::logic_error("GameStatistics::collectResult is not implemented yet.");
+  GameResult gameResult = {attempts, won, mode};
+  m_results.push_back(gameResult);
+  // throw std::logic_error("GameStatistics::collectResult is not implemented yet.");
 }
 
 std::string GameStatistics::encryptData(const std::string& data, int encryptionKey) {
@@ -44,7 +52,8 @@ std::string GameStatistics::decryptData(const std::string& data, int encryptionK
 
 
 bool GameStatistics::saveToFile(const std::string& filename) {
-  throw std::logic_error("GameStatistics::saveToFile is not implemented yet.");
+  
+  // throw std::logic_error("GameStatistics::saveToFile is not implemented yet.");
 }
 
 
@@ -64,29 +73,62 @@ std::map<std::string, int> GameStatistics::loadFromFile(const std::string& filen
 
 
 int GameStatistics::getTotalGames(std::optional<GameFactory::GameMode> mode) const {
-  throw std::logic_error("GameStatistics::getTotalGames is not implemented yet.");
+  int gamesNum = {};
+  if(!mode.has_value()){
+    return m_results.size();
+  }
+  for(int i=0; i<m_results.size(); i++){
+    if(mode == m_results[i].mode){
+      gamesNum++;
+    }
+  }
+  return gamesNum;
+  // throw std::logic_error("GameStatistics::getTotalGames is not implemented yet.");
 }
 
 
 
 int GameStatistics::getWonGames(std::optional<GameFactory::GameMode> mode) const {
-  throw std::logic_error("GameStatistics::getWonGames is not implemented yet.");
+  int gamesNum = {};
+  if(!mode.has_value()){
+    for(int i=0; i<m_results.size(); i++){
+      if(m_results[i].won){
+        gamesNum++;
+    }
+  } 
+    return m_results.size();
+  }
+  for(int i=0; i<m_results.size(); i++){
+    if(mode == m_results[i].mode && m_results[i].won){
+      gamesNum++;
+    }
+  }
+  return gamesNum;
+  // throw std::logic_error("GameStatistics::getWonGames is not implemented yet.");
 }
 
 
 
 int GameStatistics::getLostGames(std::optional<GameFactory::GameMode> mode) const {
-  throw std::logic_error("GameStatistics::getLostGames is not implemented yet.");
+  int lostGamseNum {0};
+  if(!mode.has_value()){
+    lostGamseNum = getTotalGames()-getWonGames();
+  }
+  lostGamseNum = getTotalGames(mode)-getWonGames(mode);
+  return lostGamseNum;
+  // throw std::logic_error("GameStatistics::getLostGames is not implemented yet.");
 }
 
 
 
 double GameStatistics::getWinRate(std::optional<GameFactory::GameMode> mode) const {
-  throw std::logic_error("GameStatistics::getWinRate is not implemented yet.");
+  return getWonGames(mode)/getTotalGames();
+  // throw std::logic_error("GameStatistics::getWinRate is not implemented yet.");
 }
 
 void GameStatistics::clear() {
-  throw std::logic_error("GameStatistics::clear is not implemented yet.");
+  m_results.clear();
+  // throw std::logic_error("GameStatistics::clear is not implemented yet.");
 }
 
 void GameStatistics::statisticMenu() {
